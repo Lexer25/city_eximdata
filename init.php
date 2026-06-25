f272b1b917d89b769eb4ff6a8a116809fee0f7e5
Андрей <andrey.buharov@gmail.com>
2026-06-25 11:17:11 +0300

    Добавил меню

--- init.php
+++ init.php
@@ -1,2 +1,11 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 defined('EXIMDATA_VERSION') OR define('EXIMDATA_VERSION', '2.0.0');
+
+Kohana::$config->load('menu')
+    ->set('eximdata', array(
+        'title' => 'Экспорт/импорт',
+        'url' => 'eximdata',
+        'icon' => 'fa-cog',
+        'order' => 100,
+       
+    ));
